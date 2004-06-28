2ad2acf3bce67aac69cc69b1a061abce4a7a5dea
vlm <vlm@59561ff5-6e30-0410-9f3c-9617f08c8826>
2004-06-28 21:25:18 +0000

    46 test case
    git-svn-id: https://asn1c.svn.sourceforge.net/svnroot/asn1c/trunk@45 59561ff5-6e30-0410-9f3c-9617f08c8826

---
+++ asn1c/tests/check-46.c
@@ -0,0 +1,46 @@
+#undef	NDEBUG
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <assert.h>
+
+#include <T.h>
+
+uint8_t buf1[] = {
+	32 | ((2 << 6) + 3),	/* [3], constructed */
+	5,
+	((2 << 6) + 5),	/* [5], primitive */
+	3,	/* L */
+  'a',
+  'b',
+  'c',
+};
+
+static void
+check(uint8_t *buf, int size, size_t consumed) {
+	T_t t, *tp;
+	ber_dec_rval_t rval;
+
+	tp = memset(&t, 0, sizeof(t));
+
+	fprintf(stderr, "Buf %p\n", buf);
+	rval = ber_decode(&asn1_DEF_T, (void **)&tp, buf, size);
+	fprintf(stderr, "Returned code %d, consumed %d\n",
+		(int)rval.code, (int)rval.consumed);
+
+	assert(rval.code == RC_OK);
+	assert(rval.consumed == consumed);
+}
+
+int
+main(int ac, char **av) {
+
+	(void)ac;	/* Unused argument */
+	(void)av;	/* Unused argument */
+
+	check(buf1, sizeof(buf1), sizeof(buf1));
+
+	return 0;
+}
+
