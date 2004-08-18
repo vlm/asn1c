3140e0e484a52268b50d71b32058d3a1632ac69d
Lev Walkin <vlm@lionet.info>
2004-08-18 04:50:37 +0000

    debug constraints

--- libasn1print/asn1print.h
+++ libasn1print/asn1print.h
@@ -1,15 +1,16 @@
 #ifndef	_ASN1PRINT_H_
 #define	_ASN1PRINT_H_
 
-enum asn1print_flags_e {
-	APF_NOFLAGS		= 0x00,
-	APF_LINE_COMMENTS	= 0x01,
+enum asn1print_flags {
+	APF_NOFLAGS,
+	APF_LINE_COMMENTS	= 0x01, /* Include line comments */
+	APF_DEBUG_CONSTRAINTS	= 0x02,	/* Explain constraints */
 };
 
 /*
  * Print the contents of the parsed ASN.1 syntax tree.
  */
-int asn1print(asn1p_t *asn, enum asn1print_flags_e flags);
+int asn1print(asn1p_t *asn, enum asn1print_flags flags);
 
 
 #endif	/* _ASN1PRINT_H_ */
