e3a3156505e853804d3eba90e66979fc74936256
vlm <vlm@59561ff5-6e30-0410-9f3c-9617f08c8826>
2004-09-24 20:58:37 +0000

    dedicated asn_application.h header
    git-svn-id: https://asn1c.svn.sourceforge.net/svnroot/asn1c/trunk@389 59561ff5-6e30-0410-9f3c-9617f08c8826

---
+++ skeletons/asn_application.h
@@ -0,0 +1,25 @@
+/*-
+ * Copyright (c) 2004 Lev Walkin <vlm@lionet.info>. All rights reserved.
+ * Redistribution and modifications are permitted subject to BSD license.
+ */
+/*
+ * Application-level ASN.1 API.
+ */
+#ifndef	_ASN_APPLICATION_H_
+#define	_ASN_APPLICATION_H_
+
+#include <asn_types.h>		/* for platform-dependent types */
+
+/*
+ * Generic type of an application-defined callback to return various
+ * types of data to the application.
+ * EXPECTED RETURN VALUES:
+ *  -1: Failed to consume bytes. Abort the mission.
+ * Non-negative return values indicate success, and ignored.
+ */
+typedef int (asn_app_consume_bytes_f)(const void *buffer, size_t size,
+	void *application_specific_key);
+
+#include <constr_TYPE.h>	/* for asn1_TYPE_descriptor_t */
+
+#endif	/* _ASN_APPLICATION_H_ */
