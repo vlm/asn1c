c9f64e6f69faf69ed0ef78e9ae65c60ed0375cad
Lev Walkin <vlm@lionet.info>
2004-09-24 20:54:48 +0000

    added file and line number information

--- examples/sample.source.PKIX1/config.h
+++ examples/sample.source.PKIX1/config.h
@@ -4,5 +4,6 @@ extern int opt_debug;
 #define	ASN_DEBUG(fmt, args...)	do {		\
 		if(opt_debug < 2) break;	\
 		fprintf(stderr, fmt, ##args);	\
-		fprintf(stderr, "\n");		\
+		fprintf(stderr, " (%s:%d)\n",	\
+			__FILE__, __LINE__);	\
 	} while(0)
