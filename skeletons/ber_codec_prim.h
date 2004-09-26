e73187936b9f79995f6ad4bc5b54ba45e837c29e
Lev Walkin <vlm@lionet.info>
2004-09-26 13:11:01 +0000

    generic primitive codec added

---
+++ skeletons/ber_codec_prim.h
@@ -0,0 +1,19 @@
+/*-
+ * Copyright (c) 2004 Lev Walkin <vlm@lionet.info>. All rights reserved.
+ * Redistribution and modifications are permitted subject to BSD license.
+ */
+#ifndef	_BER_CODEC_of_PRIMITIVE_TYPE_H_
+#define	_BER_CODEC_of_PRIMITIVE_TYPE_H_
+
+#include <asn_application.h>
+
+typedef struct ASN__PRIMITIVE_TYPE_s {
+	uint8_t *buf;	/* Buffer with consecutive primitive encoding bytes */
+	int size;	/* Size of the buffer */
+} ASN__PRIMITIVE_TYPE_t;	/* Do not use this type directly! */
+
+asn_struct_free_f ASN__PRIMITIVE_TYPE_free;
+ber_type_decoder_f ber_decode_primitive;
+der_type_encoder_f der_encode_primitive;
+
+#endif	/* _BER_CODEC_of_PRIMITIVE_TYPE_H_ */
