b8ec15dc188b472b3212a71238eb5338b851c39b
Lev Walkin <vlm@lionet.info>
2004-10-22 08:20:32 +0000

    NULL XER encoding

--- skeletons/NULL.h
+++ skeletons/NULL.h
@@ -17,6 +17,7 @@ extern asn_TYPE_descriptor_t asn_DEF_NULL;
 
 asn_struct_print_f NULL_print;
 der_type_encoder_f NULL_encode_der;
+xer_type_decoder_f NULL_decode_xer;
 xer_type_encoder_f NULL_encode_xer;
 
 #endif	/* NULL_H */
