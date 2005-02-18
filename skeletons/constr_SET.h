ec8b3790a663b996fe5cfd72568778d6955d3b88
vlm <vlm@59561ff5-6e30-0410-9f3c-9617f08c8826>
2005-02-18 18:45:01 +0000

    duplicate
    git-svn-id: https://asn1c.svn.sourceforge.net/svnroot/asn1c/trunk@679 59561ff5-6e30-0410-9f3c-9617f08c8826

--- skeletons/constr_SET.h
+++ skeletons/constr_SET.h
@@ -47,7 +47,6 @@ asn_constr_check_f SET_constraint;
 ber_type_decoder_f SET_decode_ber;
 der_type_encoder_f SET_encode_der;
 xer_type_decoder_f SET_decode_xer;
-xer_type_decoder_f SET_decode_xer;
 xer_type_encoder_f SET_encode_xer;
 
 /***********************
