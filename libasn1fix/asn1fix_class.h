51aed8812c1cf2d0a6399ae709ea616384ffeb11
vlm <vlm@59561ff5-6e30-0410-9f3c-9617f08c8826>
2006-03-14 15:53:59 +0000

    *** empty log message ***
    git-svn-id: https://asn1c.svn.sourceforge.net/svnroot/asn1c/trunk@1070 59561ff5-6e30-0410-9f3c-9617f08c8826

--- libasn1fix/asn1fix_class.h
+++ libasn1fix/asn1fix_class.h
@@ -12,4 +12,10 @@ asn1p_expr_t *asn1f_class_access(arg_t *, asn1p_module_t *mod, asn1p_ref_t *);
 asn1p_expr_t *asn1f_class_access2(asn1p_t *asn, asn1p_module_t *mod,
 	asn1p_expr_t *expr, asn1p_ref_t *);
 
+/*
+ * CLASS may contain the "WITH SYNTAX" clause, in which case we are
+ * going to parse it.
+ */
+int asn1f_parse_class_with_syntax(arg_t *arg);
+
 #endif	/* _ASN1FIX_CLASS_H_ */
