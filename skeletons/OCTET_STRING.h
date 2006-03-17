3fe1e517b604cb418e95278d036e8dd8f30b55cd
vlm <vlm@59561ff5-6e30-0410-9f3c-9617f08c8826>
2006-03-17 18:34:10 +0000

    *** empty log message ***
    git-svn-id: https://asn1c.svn.sourceforge.net/svnroot/asn1c/trunk@1090 59561ff5-6e30-0410-9f3c-9617f08c8826

--- skeletons/OCTET_STRING.h
+++ skeletons/OCTET_STRING.h
@@ -38,7 +38,7 @@ per_type_decoder_f OCTET_STRING_decode_uper;
 
 /*
  * This function clears the previous value of the OCTET STRING (if any)
- * and then allocates a new memory and returns a pointer to it.
+ * and then allocates a new memory with the specified content (str/size).
  * If size = -1, the size of the original string will be determined
  * using strlen(str).
  * If str equals to NULL, the function will silently clear the
