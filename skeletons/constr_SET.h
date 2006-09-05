e745fcdc3a8f971892891ab5af2ea85f61ec3782
vlm <vlm@59561ff5-6e30-0410-9f3c-9617f08c8826>
2006-09-05 16:18:34 +0000

    support parsing realnumber tokens in constraints
    git-svn-id: https://asn1c.svn.sourceforge.net/svnroot/asn1c/trunk@1152 59561ff5-6e30-0410-9f3c-9617f08c8826

--- skeletons/constr_SET.h
+++ skeletons/constr_SET.h
@@ -52,6 +52,8 @@ ber_type_decoder_f SET_decode_ber;
 der_type_encoder_f SET_encode_der;
 xer_type_decoder_f SET_decode_xer;
 xer_type_encoder_f SET_encode_xer;
+per_type_decoder_f SET_decode_uper;
+per_type_encoder_f SET_encode_uper;
 
 /***********************
  * Some handy helpers. *
