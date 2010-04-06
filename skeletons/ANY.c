79d67b89c51d12fad8a7c607e394754225a79dae
Santiago Carot-Nemesio <sancane@gmail.com>
2010-04-06 20:59:12 +0200

    Added to myself in copyright. Tested ANY DEFINED BY.

--- skeletons/ANY.c
+++ skeletons/ANY.c
@@ -1,5 +1,7 @@
 /*-
- * Copyright (c) 2004 Lev Walkin <vlm@lionet.info>. All rights reserved.
+ * Copyright (c) 2004 Lev Walkin <vlm@lionet.info>.
+ * Copyright (c) 2010 Santiago Carot-Nemesio <sancane@gmail.com>.
+ * All rights reserved.
  * Redistribution and modifications are permitted subject to BSD license.
  */
 #include <asn_internal.h>
