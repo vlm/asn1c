a7591b5c7fc4775fc12cc2e1b407ed436b298c73
Lev Walkin <vlm@lionet.info>
2014-10-12 18:37:35 -0700

    regenerated tests based on Wim L patch

--- skeletons/PrintableString.c
+++ skeletons/PrintableString.c
@@ -19,7 +19,7 @@ static const int _PrintableString_alphabet[256] = {
  0,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,	/*  abcdefghijklmno */
 64,65,66,67,68,69,70,71,72,73,74, 0, 0, 0, 0, 0,	/* pqrstuvwxyz      */
 };
-static const int _PrintableString_code2value[74] = { 
+static const int _PrintableString_code2value[74] = {
 32,39,40,41,43,44,45,46,47,48,49,50,51,52,53,54,
 55,56,57,58,61,63,65,66,67,68,69,70,71,72,73,74,
 75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,
