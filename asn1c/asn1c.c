fa40976f4ab29fc2dd3b163afc433aa3dcc871b7
Lev Walkin <vlm@lionet.info>
2017-03-26 06:37:38 -0700

    reflect contributors

--- asn1c/asn1c.c
+++ asn1c/asn1c.c
@@ -1,6 +1,6 @@
-/*-
- * Copyright (c) 2003-2016
- * 	Lev Walkin <vlm@lionet.info>. All rights reserved.
+/*
+ * Copyright (c) 2003-2017 Lev Walkin <vlm@lionet.info> and contributors.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
 #include "sys-common.h"
 
 #undef COPYRIGHT
-#define COPYRIGHT "Copyright (c) 2003-2016 Lev Walkin <vlm@lionet.info>\n"
+#define COPYRIGHT "Copyright (c) 2003-2017 Lev Walkin <vlm@lionet.info> and contributors.\n"
 
 #include <asn1parser.h>   /* Parse the ASN.1 file and build a tree */
 #include <asn1fix.h>      /* Fix the ASN.1 tree */
