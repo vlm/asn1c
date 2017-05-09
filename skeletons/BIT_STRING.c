5f619fb25810d30a250f8a20ef0782fa142ef57c
johvik <johvik@users.noreply.github.com>
2017-05-09 09:05:04 +0200

    Add const to strings in skeletons/
    This removes the GCC warning:
    "warning: initialization discards ‘const’ qualifier from pointer
    target type [-Wdiscarded-qualifiers]"
    from BIT_STRING.c and ber_tlv_tag.c.

--- skeletons/BIT_STRING.c
+++ skeletons/BIT_STRING.c
@@ -67,7 +67,7 @@ BIT_STRING_constraint(asn_TYPE_descriptor_t *td, const void *sptr,
 	return 0;
 }
 
-static char *_bit_pattern[16] = {
+static const char *_bit_pattern[16] = {
 	"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111",
 	"1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"
 };
