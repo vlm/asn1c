4558ad0629a7f3bf8d5134caf87145319149ad5c
Lev Walkin <vlm@lionet.info>
2017-07-10 05:21:30 -0700

    add OER encoder helpers

---
+++ skeletons/oer_encoder.c
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2017 Lev Walkin <vlm@lionet.info>. All rights reserved.
+ * Redistribution and modifications are permitted subject to BSD license.
+ */
+#include <asn_internal.h>
+
+/*
+ * The OER encoder of any type.
+ */
+asn_enc_rval_t
+oer_encode(asn_TYPE_descriptor_t *type_descriptor, void *struct_ptr,
+        asn_app_consume_bytes_f *consume_bytes, void *app_key) {
+
+        ASN_DEBUG("OER encoder invoked for %s",
+                type_descriptor->name);
+
+        /*
+         * Invoke type-specific encoder.
+         */
+        return type_descriptor->oer_encoder(type_descriptor, 0,
+                struct_ptr,     /* Pointer to the destination structure */
+                consume_bytes, app_key);
+}
+
+/*
+ * Argument type and callback necessary for oer_encode_to_buffer().
+ */
+typedef struct enc_to_buf_arg {
+        void *buffer;
+        size_t left;
+} enc_to_buf_arg;
+static int encode_to_buffer_cb(const void *buffer, size_t size, void *key) {
+        enc_to_buf_arg *arg = (enc_to_buf_arg *)key;
+
+        if(arg->left < size)
+                return -1;      /* Data exceeds the available buffer size */
+
+        memcpy(arg->buffer, buffer, size);
+        arg->buffer = ((char *)arg->buffer) + size;
+        arg->left -= size;
+
+        return 0;
+}
+
+/*
+ * A variant of the oer_encode() which encodes the data into the provided buffer
+ */
+asn_enc_rval_t
+oer_encode_to_buffer(struct asn_TYPE_descriptor_s *type_descriptor,
+                     void *struct_ptr,  /* Structure to be encoded */
+                     void *buffer,      /* Pre-allocated buffer */
+                     size_t buffer_size /* Initial buffer size (maximum) */
+                     ) {
+        enc_to_buf_arg arg;
+        asn_enc_rval_t ec;
+
+        arg.buffer = buffer;
+        arg.left = buffer_size;
+
+        ec = type_descriptor->oer_encoder(
+            type_descriptor, 0,
+            struct_ptr, /* Pointer to the destination structure */
+            encode_to_buffer_cb, &arg);
+        if(ec.encoded != -1) {
+                assert(ec.encoded == (ssize_t)(buffer_size - arg.left));
+                /* Return the encoded contents size */
+        }
+        return ec;
+}
--- skeletons/oer_encoder.h
+++ skeletons/oer_encoder.h
@@ -18,28 +18,29 @@ struct asn_TYPE_descriptor_s;	/* Forward declaration */
  * This function may be invoked directly by the application.
  */
 asn_enc_rval_t oer_encode(struct asn_TYPE_descriptor_s *type_descriptor,
-		void *struct_ptr,	/* Structure to be encoded */
-		asn_app_consume_bytes_f *consume_bytes_cb,
-		void *app_key		/* Arbitrary callback argument */
-	);
+                          void *struct_ptr, /* Structure to be encoded */
+                          asn_app_consume_bytes_f *consume_bytes_cb,
+                          void *app_key /* Arbitrary callback argument */
+                          );
 
 /* A variant of oer_encode() which encodes data into the pre-allocated buffer */
 asn_enc_rval_t oer_encode_to_buffer(
-		struct asn_TYPE_descriptor_s *type_descriptor,
-		void *struct_ptr,	/* Structure to be encoded */
-		void *buffer,		/* Pre-allocated buffer */
-		size_t buffer_size	/* Initial buffer size (maximum) */
-	);
+    struct asn_TYPE_descriptor_s *type_descriptor,
+    void *struct_ptr,  /* Structure to be encoded */
+    void *buffer,      /* Pre-allocated buffer */
+    size_t buffer_size /* Initial buffer size (maximum) */
+    );
 
 /*
  * Type of the generic OER encoder.
  */
-typedef asn_enc_rval_t (oer_type_encoder_f)(
-		struct asn_TYPE_descriptor_s *type_descriptor,
-		void *struct_ptr,	/* Structure to be encoded */
-		asn_app_consume_bytes_f *consume_bytes_cb,	/* Callback */
-		void *app_key		/* Arbitrary callback argument */
-	);
+typedef asn_enc_rval_t(oer_type_encoder_f)(
+    struct asn_TYPE_descriptor_s *type_descriptor,
+    asn_oer_constraints_t *constraints,
+    void *struct_ptr,                          /* Structure to be encoded */
+    asn_app_consume_bytes_f *consume_bytes_cb, /* Callback */
+    void *app_key                              /* Arbitrary callback argument */
+    );
 
 
 #ifdef __cplusplus
