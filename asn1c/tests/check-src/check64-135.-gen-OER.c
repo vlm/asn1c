b2284470f9c3ac437c720bc838972e824b77cdd3
Lev Walkin <vlm@lionet.info>
2017-07-14 15:18:30 +0400

    add OER INTEGER constraint check

---
+++ asn1c/tests/check-src/check64-135.-gen-OER.c
@@ -0,0 +1,22 @@
+/*
+ * Verify OER with constrained INTEGER code gen.
+ */
+#undef	NDEBUG
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+
+#include <T.h>
+
+int main() {
+	T_t t;
+
+    memset(&t, 0, sizeof(t));
+
+	return 0;
+}
+
