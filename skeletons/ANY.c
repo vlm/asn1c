86f58b8d5638f3db5c5c1dddb3405cbe6f0123ee
lei sun <lsun@sierrawireless.com>
2017-07-17 13:30:24 -0700

    add missing uper handler in ANY.c

--- skeletons/ANY.c
+++ skeletons/ANY.c
@@ -21,7 +21,8 @@ asn_TYPE_descriptor_t asn_DEF_ANY = {
 	OCTET_STRING_encode_der,
 	OCTET_STRING_decode_xer_hex,
 	ANY_encode_xer,
-	0, 0,
+    OCTET_STRING_decode_uper,
+    OCTET_STRING_encode_uper,
     OCTET_STRING_decode_oer,
     OCTET_STRING_encode_oer,
 	0, /* Use generic outmost tag fetcher */
