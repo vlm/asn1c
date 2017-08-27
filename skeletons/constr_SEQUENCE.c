878a521e2b4529f155e6ee495572bb6f964d8200
Lev Walkin <vlm@lionet.info>
2017-08-27 12:39:16 -0700

    initialize all fields

--- skeletons/constr_SEQUENCE.c
+++ skeletons/constr_SEQUENCE.c
@@ -312,9 +312,7 @@ SEQUENCE_decode_ber(asn_codec_ctx_t *opt_codec_ctx, asn_TYPE_descriptor_t *td,
 			 * sorted array of tags.
 			 */
 			const asn_TYPE_tag2member_t *t2m;
-			asn_TYPE_tag2member_t key;
-			key.el_tag = tlv_tag;
-			key.el_no = edx;
+			asn_TYPE_tag2member_t key = {tlv_tag, edx, 0, 0};
 			t2m = (const asn_TYPE_tag2member_t *)bsearch(&key,
 				specs->tag2el, specs->tag2el_count,
 				sizeof(specs->tag2el[0]), _t2e_cmp);
