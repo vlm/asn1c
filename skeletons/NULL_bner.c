7790229ae714ad224913417df7d7160a6a431dc4
Jon Ringle <jringle@gridpoint.com>
2017-11-21 08:43:15 -0500

    BNER support: Add skeletons/*_bner.c with stub functions

---
+++ skeletons/NULL_bner.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2003-2017 Lev Walkin <vlm@lionet.info> and contributors.
+ * All rights reserved.
+ * Redistribution and modifications are permitted subject to BSD license.
+ */
+#include <NULL.h>
+#include <asn_internal.h>
+
+asn_dec_rval_t
+NULL_decode_bner(const asn_codec_ctx_t *opt_codec_ctx,
+                 const asn_TYPE_descriptor_t *td, void **struct_ptr,
+                 const void *buf_ptr, size_t size, ber_tlv_tag_t tag,
+                 int tag_mode) {
+    (void)opt_codec_ctx;
+    (void)td;
+    (void)struct_ptr;
+    (void)buf_ptr;
+    (void)size;
+    (void)tag;
+    (void)tag_mode;
+
+    asn_dec_rval_t tmp_error = {RC_FAIL, 0};
+    ASN_DEBUG("%s Not yet implemented. Failed to decode %s", __func__,
+              td->name);
+    return tmp_error;
+}
+
+asn_enc_rval_t
+NULL_encode_bner(const asn_TYPE_descriptor_t *td, const void *sptr,
+                 int tag_mode, ber_tlv_tag_t tag, asn_app_consume_bytes_f *cb,
+                 void *app_key) {
+    (void)td;
+    (void)sptr;
+    (void)tag_mode;
+    (void)tag;
+    (void)cb;
+    (void)app_key;
+
+    ASN_DEBUG("%s Not yet implemented. Failed to encode %s", __func__,
+              td->name);
+    ASN__ENCODE_FAILED;
+}
