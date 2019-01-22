ccfaf2ab896936f2803827662c7ce1c58ae24e41
Lev Walkin <vlm@lionet.info>
2019-01-21 17:45:31 -0800

    weeded the library out of unber program

---
+++ asn1-tools/unber/libasn1_unber_tool.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2004-2019 Lev Walkin <vlm@lionet.info>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+/* Set global options. */
+void set_minimalistic_output(int v);
+void set_single_type_decoding(int v);
+void set_pretty_printing(int v);
+int set_skip_bytes(long v);
+int set_indent_size(int indent_size);
+
+/*
+ * Convert BER-encoded file into the low level non-standard XML-like structure.
+ * Primarily used for manual debugging.
+ */
+int unber_file(const char *fname);
+
+/*
+ * Decode the TLV given by the given string.
+ */
+int decode_tlv_from_hex_string(const char *datastring);
+
